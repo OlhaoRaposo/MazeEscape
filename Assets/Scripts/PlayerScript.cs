50f83265d29518b20a136170bc1554c482b1481f
Gabriel Viana <gabrielvianafonseca1590@gmail.com>
2022-09-04 16:12:47 -0300

    Stamina&Audios
    Adicionado a estamina do player e audios de destrancar, alavanca, corpo caindo, pegar chave e respiração

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,17 +10,18 @@ public class PlayerScript : MonoBehaviour
         public Transform groundCheck;
         public int keys;
         
-        
     //Private Var
         private float groundDistance = 0.06f;
         private float jumpHeight;       
         private bool isGround;
         private float gravity = -19.20f;
-        private float speed = 3;
+        private float speed = 2;
         private bool isWalking;
         private bool IsCoroutineRunning = false;
         private bool isCheck;
         private bool isRuning = false;
+        private bool stamina = true;
+        private bool canRun = true;
         Vector3 velocity;
         private void Start() {
             FindObjectOfType<AudioManager>().Play("StaticSound");
@@ -61,18 +62,23 @@ public class PlayerScript : MonoBehaviour
                 velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
             }
 
-            if (Input.GetKeyDown(KeyCode.LeftShift))
+            if (Input.GetKeyDown(KeyCode.LeftShift) && stamina == true)
             {
                 if(!isRuning)
-                    speed += 2;
+                    speed += 4;
+                    canRun = false;
+                    stamina = false;
+                    Invoke("StaminaMarker", 6f);
                 isRuning = true;
             }
             if (Input.GetKeyUp(KeyCode.LeftShift))
             {
                 if(isRuning)
-                    speed -= 2;
+                    speed = 2;
                 isRuning = false;
-            }
+                
+        }
+        Debug.Log(speed);
         }
         void CheckSound() {
             if(!IsCoroutineRunning) {
@@ -94,5 +100,20 @@ public class PlayerScript : MonoBehaviour
             }
             IsCoroutineRunning = false;
         }
-        
+
+        void StaminaMarker()
+        {   
+            speed = 2;
+
+            if (stamina == false && canRun == false)
+                {
+                    canRun = true;
+                    FindObjectOfType<AudioManager>().Play("Breathe");
+                    Invoke("StaminaMarker", 5f);
+                }
+            if (canRun == true)
+            {
+                stamina = true;
+            }
+        }
 }
