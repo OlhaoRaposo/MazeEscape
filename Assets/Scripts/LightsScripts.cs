c29b74087e88445259dd2fdfa11d3e9198742a67
Gabriel Viana <gabrielvianafonseca1590@gmail.com>
2022-09-04 18:15:05 -0300

    Finalização(Em ingles)
    Botei os joint, parede e porta certinho, teto com as lampadas e tals

---
+++ Assets/Scripts/LightsScripts.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsScripts : MonoBehaviour
+{
+    public Animator[] anim;
+    int random; 
+    void Start()
+    {
+        for (int i = 0; i < 0; i++)
+        {
+
+            random = Random.Range(0, 101);
+            if (random <= 60)
+            {
+                anim[i].SetBool("isOn", true);
+            }
+            else if (random > 60 && random <= 75)
+            {
+                anim[i].SetBool("isOff", true);
+            }
+            else
+            {
+                anim[i].SetBool("isBlinking", true);
+            }
+        }
+    }
+}
